47011a26366768c77dc4fbfdc5e99b2d9f7cc92b
Charlotte <hunandy14@gmail.com>
2021-05-23 02:30:27 +0900

    修正String版本

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -2,66 +2,65 @@
 #include <vector>
 #include <fstream>
 #include <string>
-#include <string_view>
-#include <charconv>
 using namespace std;
 
 class OneLine {
-private:
-	using _type = vector<string_view>;
+public:
+	using _type = string;
+	using _typev = vector<_type>;
 public:
 	OneLine() {}
-	OneLine(_type oneline) {
-		data = oneline;
+	OneLine(_typev oneline) {
+		_data = oneline;
 	}
-	OneLine(const string_view strv, string_view delims = " ") {
-		splitSV(strv, delims);
+	OneLine(const _type strv, _type delims) {
+		split(strv, delims);
 	}
-	operator _type() const {
-		return data;
+	operator _typev() const {
+		return _data;
 	}
-	operator vector<string>() const {
-		vector<string> str(data.size());
-		for (size_t i = 0; i < data.size(); i++)
-			str[i] = data[i];
+	operator string() {
 		return str;
 	}
-	string getStringIdx(size_t idx) const {
-		string s = "";
-		if (idx < data.size()) {
-			s = string(data[idx]);
-		} else {
-			throw runtime_error("range Error.");
-		}
-		return s;
-	}
-	long getIntIdx(size_t idx) const {
-		return stoull(getStringIdx(idx));
+	const _type& operator[](size_t idx) const {
+		return _data[idx];
 	}
 	size_t size() {
-		return data.size();
+		return _data.size();
 	}
-	_type splitSV(string_view strv, string_view delims = " ") {
-		_type output;
+public:
+	const _typev& split(_type strv, _type delims = " ") {
+		_typev output;
 		for (size_t first = 0; first < strv.size();) {
 			const auto second = strv.find_first_of(delims, first);
 			if (first != second)
 				output.emplace_back(strv.substr(first, second - first));
-			if (second == string_view::npos)
+			if (second == _type::npos)
 				break;
 			first = second + 1;
 		}
 		if (output.size() == 0)
 			output.emplace_back("");
-		data = std::move(output);
-		return data;
+		_data = std::move(output);
+		return _data;
 	}
-	std::istream& getline(string_view delims = " ") {
-		std::istream& is = std::getline(fs, str);
-		if (is) {
-			splitSV(str, delims);
+	bool readNextLine(_type delims = " ") {
+		bool notend=0;
+		if (fs.is_open()) 	{
+			std::istream& is = std::getline(fs, str);
+			if (is) {
+				split(str, delims);
+				notend=1;
+			}
+		} else {
+			std::istream& is = std::getline(std::cin, str);
+			if (is) {
+				split(str, delims);
+				notend=1;
+			}
 		}
-		return is;
+
+		return notend;
 	}
 	void openFile(const string file_name) {
 		if (name != file_name) {
@@ -72,14 +71,17 @@ public:
 				throw runtime_error("Reading error.");
 		}
 	}
+	void closeFile() {
+		fs.close();
+	}
 private:
-	_type data;
+	_typev _data;
 	string str;
 private:
 	string name;
 	fstream fs;
 };
-std::ostream& operator<<(std::ostream& out, vector<string_view>& v) {
+std::ostream& operator<<(std::ostream& out, OneLine& v) {
 	cout << "[";
 	for (size_t i = 0; i < v.size(); i++) {
 		out << v[i];
@@ -88,71 +90,42 @@ std::ostream& operator<<(std::ostream& out, vector<string_view>& v) {
 	cout << "]";
 	return out;
 }
-std::ostream& operator<<(std::ostream& out, OneLine& v) {
-	return out << (vector<string_view>&)v;
-}
 
 //=============================================================================
-// ³v¦æÅª¨ú
-vector<string> ReadFile_line(const string file_name) {
-	vector<string> v;
-	fstream fs(file_name, ios::in);
-	if (!fs.is_open())
-		throw runtime_error("Reading error.");
-
-	for (string str; getline(fs, str);) {
-		//cout << str << endl;
-		v.emplace_back(str);
-	} fs.seekg(0, fs.beg);
-	fs.close();
-	return v;
-}
-
-// ¤Á³ÎÀÉ®×
-void rf_test1() {
-	auto&& v1 = ReadFile_line("in.txt");
-	vector<OneLine> v2;
-	for (auto&& i : v1) {
-		v2.emplace_back(OneLine(i, " "));
-	}
-	for (auto&& i : v2) {
-		cout << i << endl;
-	}
-}
-// ¤Á³ÎÀÉ®× (Ãi¥[¸ü)
-void rf_test2() {
+// ±qÀÉ®×¸ü¤J¨Ã¤Á³Î
+void OneLine_test1() {
+	cout << "=====================±qÀÉ®×¸ü¤J¨Ã¤Á³Î=====================" << endl;
 	OneLine line;
-	line.openFile("a.txt");
-	while (line.getline()) {
-		vector<string_view>&& tokenList = line;
-		cout << tokenList << endl;
+	line.openFile("data_test.txt");
+	while (line.readNextLine(" ")) {
+		cout << line << endl;
 	}
+	line.closeFile();
 }
 // ¤Á³Î¦r¦ê
-void rf_test3() {
+void OneLine_test2() {
+	cout << "=====================±q¿é¤J¸ü¤J¨Ã¤Á³Î=====================" << endl;
+	OneLine line;
+	line.readNextLine(" ");
+	cout << line << endl;
+}
+// ¤Á³Î¦r¦ê
+void OneLine_test3() {
+	cout << "=====================¡@¡@¤Á³Î¦r¦ê¡@¡@=====================" << endl;
 	string str = "123 | 321";
 	OneLine line(str, " | ");
 	cout << line << endl;
-
-	vector<string_view>&& tokenList = line;
-	for (size_t i = 0; i < tokenList.size(); i++)
-		cout << tokenList[i] << endl;
-}
-
-// ªð¦^¦r¦ê
-void rf_test_getIdx() {
-	string str = "123 | 321";
-	OneLine line(str, " | ");
-
-	cout << line.getStringIdx(2) << endl;
+	for (size_t i = 0; i < line.size(); i++)
+		cout << "line[" << i << "] = " << line[i] << endl;
 }
 
 
 //====================================================================================
 int main(int argc, char const* argv[]) {
-	rf_test1();
-	//rf_test2();
-	//rf_test3();
+	OneLine_test1();
+	OneLine_test2();
+	OneLine_test3();
+	//OneLine_test4();
 	return 0;
 }
 //====================================================================================
